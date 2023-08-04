9a908baf0aba559c3f4baf75b6b22bd8718cbf45
k4r1-k450 <67853130+k4r1-k450@users.noreply.github.com>
2023-08-04 15:54:55 +0900

    ラジオボタンで変換先を指定できるようにした

--- popup.js
+++ popup.js
@@ -1,13 +1,33 @@
 "use strict";
 
+import reformChar from "./reformChar.js";
+
 let input = document.getElementById("input");
 let reform_button = document.getElementById("button");
 let output = document.getElementById("output");
 
-import {char_data} from "./char_data.js";
-import reformChar from "./reformChar.js";
+let font = document.getElementsByName("font");
+let type = document.getElementsByName("type");
+
+console.log(font, type);
 
 reform_button.addEventListener("click", function(){
-    var reformed = reformChar(char_data, input.value);
+    let reformType = {"font" : "", "type" : ""};
+
+    for (const fontRadio of font) {
+        if (fontRadio.checked) {
+            reformType.font = fontRadio.value;
+            break;
+        }
+    }
+
+    for (const typeRadio of type) {
+        if (typeRadio.checked) {
+            reformType.type = typeRadio.value;
+            break;
+        }
+    }
+
+    var reformed = reformChar(input.value, reformType.font, reformType.type);
     output.innerHTML = reformed;
 });
--- reformChar.js
+++ reformChar.js
@@ -1,15 +1,17 @@
-export default function reformChar(char_data, string){
+import {char_data} from "./char_data.js";
 
+export default function reformChar(string, font, type){
+    console.log(font, type);
     var reformed = "";
 
     for (let i = 0; i < string.length; i++) {
         try {
             if(string[i].match(/[A-Z]/)) {
-                reformed += String.fromCodePoint(parseInt(char_data["Upper"]["Suns_Selif"]["Bold"], 16) + (string[i].codePointAt() - "A".codePointAt()))
+                reformed += String.fromCodePoint(parseInt(char_data["Upper"][font][type], 16) + (string[i].codePointAt() - "A".codePointAt()))
             } else if(string[i].match(/[a-z]/)) {
-                reformed += String.fromCodePoint(parseInt(char_data["Lower"]["Suns_Selif"]["Bold"], 16) + (string[i].codePointAt() - "a".codePointAt()))
+                reformed += String.fromCodePoint(parseInt(char_data["Lower"][font][type], 16) + (string[i].codePointAt() - "a".codePointAt()))
             } else if(string[i].match(/[0-9]/)) {
-                reformed += String.fromCodePoint(parseInt(char_data["Number"]["Suns_Selif"]["Bold"], 16) + (string[i].codePointAt() - "0".codePointAt()))
+                reformed += String.fromCodePoint(parseInt(char_data["Number"][font][type], 16) + (string[i].codePointAt() - "0".codePointAt()))
             } else {
                 reformed += string[i];
             }
